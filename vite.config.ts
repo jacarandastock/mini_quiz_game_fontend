4eb2755ec6cf7a167c2b4b95bb92fb7ed76b16e0
Gary Du <gudu9182@uni.sydney.edu.au>
2024-03-07 01:07:09 +1100

    game

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: process.env.DEPLOY_ENV === 'gh-pages'
+        ? '/garden_party_stock_game/'
+        : '/',
     plugins: [
         VueRouter({
             /* options */
